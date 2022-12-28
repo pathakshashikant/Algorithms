072f2a0c44e50f0ab3fb2fa1be3416c23dfcf6e1
Shashikant Pathak <shashikant.pathak@zimmerpeacock.com>
2022-12-28 14:38:06 +0100

    binary search recursive approach

--- _binary_search.js
+++ _binary_search.js
@@ -25,3 +25,31 @@ console.log(binarySearch([1, 5, 7, 8], 9));
 
 // Big O: O(log(n))
 //Binary search works only on sorted array
+
+const search = (array, t, leftIndex, rightIndex) => {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+  if (t === array[middleIndex]) {
+    return middleIndex;
+  }
+
+  if (t < array[middleIndex]) {
+    return search(array, t, leftIndex, middleIndex - 1);
+  }
+  if (t > array[middleIndex]) {
+    return search(array, t, middleIndex + 1, rightIndex);
+  }
+};
+
+const binarySearchRecursive = (array, t) => {
+  return search(array, t, 0, array.length - 1);
+};
+
+console.log(binarySearchRecursive([1, 5, 7, 8], 5));
+console.log(binarySearchRecursive([1, 5, 7, 8], 9));
+
+// Big O: O(log(n))
